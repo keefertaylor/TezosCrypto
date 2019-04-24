834a882b5a183e62eedb6dc470ba2e6927bc18f2
keefertaylor <keefer.t@gmail.com>
2019-04-24 02:52:10 +0100

    Fix comments

--- TezosCrypto/OperationSigningResult.swift
+++ TezosCrypto/OperationSigningResult.swift
@@ -2,17 +2,17 @@
 
 import Foundation
 
-/** A property bag representing various artifacts from signing an operation. */
+/// A property bag representing various artifacts from signing an operation.
 public struct OperationSigningResult {
-  /** The hashed operation bytes which were produced via hashing and signed. */
+  /// The hashed operation bytes which were produced via hashing and signed.
   public let operationBytes: [UInt8]
 
-  /** The signature of the signed bytes. */
+  /// The signature of the signed bytes.
   public let signature: [UInt8]
 
-  /** The base58check encoded version of the signature, prefixed with 'edsig' */
+  /// The base58check encoded version of the signature, prefixed with 'edsig'
   public let edsig: String
 
-  /** The operation string concatenated with a hex encoded signature. */
+  /// The operation string concatenated with a hex encoded signature.
   public let sbytes: String
 }
